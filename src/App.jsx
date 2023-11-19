3cf3d812463becbef075123b2a2e85fd8e6849f2
Adejuwon Oshindoro <temitayoadejuwon@gmail.com>
2023-11-19 16:59:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './NavBar.css'
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const NavBar = () => {
+
+    const [isOpened, setIsOpened] = useState(false);
+
+    const handleClick = () => {
+        return (
+            setIsOpened(prev => !prev)
+        )
+    } 
+
+  return (
+    <div className="nav">
+        <p>
+            <span>wet</span>
+            <span>Bank</span>
+        </p>
+        <nav className={isOpened ? 'mobile-nav' : '' }>
+            <ul>
+                <li><a href="">Discover</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">FAQs</a></li>
+                <li><a href="">About US</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <button>Login / Sign Up</button>
+            
+        </nav>
+        <RxHamburgerMenu onClick={handleClick}  className='ham' size={26} />
+    </div>
+  )
+}
+
+export default NavBar
